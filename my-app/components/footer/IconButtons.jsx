dfabc7a05233f8b7a163233df29d7bcd45f04634
daiki130 <daikibaseball130@gmail.com>
2021-03-25 08:42:40 +0900

    💄 IconButtonをアップデート

--- my-app/components/footer/IconButtons.jsx
+++ my-app/components/footer/IconButtons.jsx
@@ -12,24 +12,29 @@ export const IconButtons = () => {
   return (
     <>
       <li className={`${styles.IconContainer} ${styles.faFacebook_style}`}>
-        <a href="#"></a>
-        <FontAwesomeIcon icon={Icons[0]} className={styles.IconButton} />;
+        <a href="#" className={styles.IconButtonA}>
+          <FontAwesomeIcon icon={Icons[0]} className={styles.IconButton} />
+        </a>
       </li>
       <li className={`${styles.IconContainer} ${styles.faCamera_style}`}>
-        <a href="#"></a>
-        <FontAwesomeIcon icon={Icons[1]} className={styles.IconButton} />;
+        <a href="#" className={styles.IconButtonA}>
+          <FontAwesomeIcon icon={Icons[1]} className={styles.IconButton} />
+        </a>
       </li>
       <li className={`${styles.IconContainer} ${styles.faTwitter_style}`}>
-        <a href="#"></a>
-        <FontAwesomeIcon icon={Icons[2]} className={styles.IconButton} />;
+        <a href="#" className={styles.IconButtonA}>
+          <FontAwesomeIcon icon={Icons[2]} className={styles.IconButton} />
+        </a>
       </li>
       <li className={`${styles.IconContainer} ${styles.faEnvelope_style}`}>
-        <a href="#"></a>
-        <FontAwesomeIcon icon={Icons[3]} className={styles.IconButton} />;
+        <a href="#" className={styles.IconButtonA}>
+          <FontAwesomeIcon icon={Icons[3]} className={styles.IconButton} />
+        </a>
       </li>
       <li className={`${styles.IconContainer} ${styles.faYoutube_style}`}>
-        <a href="#"></a>
-        <FontAwesomeIcon icon={Icons[4]} className={styles.IconButton} />;
+        <a href="#" className={styles.IconButtonA}>
+          <FontAwesomeIcon icon={Icons[4]} className={styles.IconButton} />
+        </a>
       </li>
     </>
   );
